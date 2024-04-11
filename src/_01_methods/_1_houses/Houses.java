b1a2db858939874b4730cfac2d80143ac019d3c2
lvl0_module2 <zoey.lestyk@gmail.com>
2024-04-10 19:55:59 -0700

    Making a city

--- src/_01_methods/_1_houses/Houses.java
+++ src/_01_methods/_1_houses/Houses.java
@@ -1,11 +1,28 @@
 package _01_methods._1_houses;
+
+import org.jointheleague.graphical.robot.Robot;
+
 /*
  *    Copyright (c) The League of Amazing Programmers 2013-2021
  *    Level 1
  */
 public class Houses {
-		
 	public void run() {
-		// Check the recipe to find out what code to put here
+		Robot rob = new Robot();
+		rob.hide();
+		rob.setRandomPenColor();
+		rob.setX(10);
+		rob.setY(590);
+		rob.penDown();
+		rob.setSpeed(1000);
+		rob.setAngle(0);
+		rob.move(100);
+		rob.turn(90);
+		rob.move(25);
+		rob.turn(90);
+		rob.move(100);
+		rob.turn(-90);
+		rob.setPenColor(0,200,0);
+		rob.move(25);
 	}
 }
