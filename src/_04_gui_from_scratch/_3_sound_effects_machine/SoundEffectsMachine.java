92236436d9cb2d1df87f39366c1efe74619d1e50
league <denizacar2625@gmail.com>
2024-08-01 19:57:17 -0700

    I finished this Module

--- src/_04_gui_from_scratch/_3_sound_effects_machine/SoundEffectsMachine.java
+++ src/_04_gui_from_scratch/_3_sound_effects_machine/SoundEffectsMachine.java
@@ -2,38 +2,79 @@ package _04_gui_from_scratch._3_sound_effects_machine;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class SoundEffectsMachine implements ActionListener{
 	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
 	JButton button = new JButton();
 	JButton button2 = new JButton();
 	JButton button3 = new JButton();
 	JButton button4 = new JButton();
 	JButton button5 = new JButton();
-	
-	
+
+
 	public void showButton() {
 		frame.setVisible(true);
-		frame.add(button);
-		frame.add(button2);
-		frame.add(button3);
-		frame.add(button4);
-		frame.add(button5);
+		frame.add(panel);
+		panel.add(button);
+		panel.add(button2);
+		panel.add(button3);
+		panel.add(button4);
+		panel.add(button5);
 		frame.pack();
 		button.addActionListener(this);
 		button2.addActionListener(this);
 		button3.addActionListener(this);
 		button4.addActionListener(this);
-		button5.addActionListener(this);
-		System.out.println("Button clicked");	
+		button5.addActionListener(this);	
+		
+	}
+	private void playSound(String soundFile) {
+		String path = "src/_04_gui_from_scratch/_3_sound_effects_machine/";
+		File sound = new File(path+soundFile);
+		if (sound.exists()) {
+			new Thread(() -> {
+				try {
+					Clip clip = AudioSystem.getClip();
+					clip.open(AudioSystem.getAudioInputStream(sound));
+					clip.start();
+					Thread.sleep(clip.getMicrosecondLength()/1000);
+				}
+				catch (Exception e) {
+					System.out.println("Could not play this sound");
+				}}).start();
+		}
+		else {
+			System.out.println("File does not exist");
+		}
+
 	}
 
 	@Override
-	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-		
+	public void actionPerformed(ActionEvent arg0) {
+		JButton buttonPressed = (JButton) arg0.getSource();
+		if (buttonPressed == button) {
+			playSound("drums.wav");
+		}
+		if (buttonPressed == button2) {
+			playSound("eat.wav");
+		}
+		if (buttonPressed == button3) {
+			playSound("horse.wav");
+		}
+		if (buttonPressed == button4) {
+			playSound("orchestra.wav");
+		}
+		if (buttonPressed == button5) {
+			playSound("sawing-wood-daniel_simon.wav");
+		}
 	}
+	
 }
